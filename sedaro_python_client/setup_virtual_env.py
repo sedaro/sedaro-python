3a29f5e1e83afb7145297f6c7c1f5f932e8c9ec8
Zachary Duvall <zachary.duvall@sedarotech.com>
2022-10-03 15:39:45 -0400

    Remove activate command

--- sedaro_python_client/setup_virtual_env.py
+++ sedaro_python_client/setup_virtual_env.py
@@ -3,7 +3,6 @@ import os
 
 def make_virtual_env():
     os.system('python3.9 -m venv ./.venv')
-    os.system('source .venv/bin/activate')
     os.system('pipenv install -r sedaro_python_client/requirements.txt')
 
 
