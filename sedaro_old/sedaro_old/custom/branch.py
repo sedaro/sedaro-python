9e42499fb8c53fafacd67ae67f4db20142a6f53b
Zachary Duvall <zachary.duvall@sedarotech.com>
2022-10-12 16:48:04 -0400

    Update Branch, add returning block group

--- sedaro_old/sedaro_old/custom/branch.py
+++ sedaro_old/sedaro_old/custom/branch.py
@@ -1,13 +1,32 @@
-from typing import Dict
+from typing import TYPE_CHECKING, Dict
 from dataclasses import dataclass
+from pydash.strings import snake_case
+
 from sedaro_old.api_client import ApiClient
+from .block_group import BlockGroup
+
+if TYPE_CHECKING:
+    from .sedaro_api_client import SedaroApiClient
 
 
 @dataclass
 class Branch:
     id: int
     data: Dict
-    sedaro_client: ApiClient
+    sedaro_client: 'SedaroApiClient'
 
     def __str__(self):
         return f'Branch(id: {self.id})'
+
+    def __getattr__(self, block_name: str) -> BlockGroup:
+        block_open_api_instance = self.sedaro_client._get_block_open_api_instance(block_name)
+        block_create_class = self.sedaro_client._get_create_or_update_block_model(
+            block_name,
+            'create'
+        )
+        return BlockGroup(
+            block_name=block_name,
+            open_api_instance=block_open_api_instance,
+            create_class=block_create_class,
+            branch_id=self.id
+        )
