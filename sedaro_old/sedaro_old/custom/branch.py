afc16b2829c910ce24f8cfd031acff5e7669cc3c
Zachary Duvall <zachary.duvall@sedarotech.com>
2022-10-12 17:00:53 -0400

    Update BlockGroupSingleClass

--- sedaro_old/sedaro_old/custom/branch.py
+++ sedaro_old/sedaro_old/custom/branch.py
@@ -1,9 +1,7 @@
 from typing import TYPE_CHECKING, Dict
 from dataclasses import dataclass
-from pydash.strings import snake_case
 
-from sedaro_old.api_client import ApiClient
-from .block_group import BlockGroup
+from .block_group_single_class import BlockGroupSingleClass
 
 if TYPE_CHECKING:
     from .sedaro_api_client import SedaroApiClient
@@ -18,15 +16,15 @@ class Branch:
     def __str__(self):
         return f'Branch(id: {self.id})'
 
-    def __getattr__(self, block_name: str) -> BlockGroup:
+    def __getattr__(self, block_name: str) -> BlockGroupSingleClass:
         block_open_api_instance = self.sedaro_client._get_block_open_api_instance(block_name)
         block_create_class = self.sedaro_client._get_create_or_update_block_model(
             block_name,
             'create'
         )
-        return BlockGroup(
+        return BlockGroupSingleClass(
             block_name=block_name,
-            open_api_instance=block_open_api_instance,
+            block_openapi_instance=block_open_api_instance,
             create_class=block_create_class,
             branch_id=self.id
         )
