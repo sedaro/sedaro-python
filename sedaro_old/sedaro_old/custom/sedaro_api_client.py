bad469ea10ebec0300a60b0edb81c7de8628a7d7
Zachary Duvall <zachary.duvall@sedarotech.com>
2022-10-12 17:15:35 -0400

    Update sedaro api client

--- sedaro_old/sedaro_old/custom/sedaro_api_client.py
+++ sedaro_old/sedaro_old/custom/sedaro_api_client.py
@@ -1,56 +1,13 @@
 from importlib import import_module
-from dataclasses import dataclass
-from typing import Dict
-from pydash.strings import snake_case, pascal_case
+from typing import Literal
 # FIXME: figure out how to require pydash dynamically.
 
 from sedaro_old.api_client import ApiClient
-from .configuration import config
 from sedaro_old.api_client import Api
-
-PACKAGE_NAME = 'sedaro_old'
-
-
-@dataclass
-class BlockClient:
-    _block_snake_case: str
-    _BlockCreate: type
-    _BlockUpdate: type
-    _branch_id: int
-    block_open_api_instance: Api
-
-    def create(self, body: Dict, **kwargs) -> Dict:  # FIXME: return value
-        """Creates a Sedaro `Block` of the given type in the Sedaro database.
-
-        Args:
-            body (Dict): a dictionary containing key/value pairs for the Sedaro `Block`
-
-        Returns:
-            _type_: _description_
-            FIXME: ^^^^^^
-        """
-        return getattr(self.block_open_api_instance, f'create_{self._block_snake_case}')(
-            body=self._BlockCreate(**body),
-            **kwargs,
-            path_params={'branchId': self._branch_id},
-        )
-
-    def update(self, block_id: str, body: Dict, **kwargs) -> Dict:  # FIXME: return value
-        """_summary_
-
-        Args:
-            block_id (str): `id` of the Sedaro `Block` to update
-            body (Dict): a dictionary containing key/value pairs to update on the Sedaro `Block`
-
-        Returns:
-            _type_: _description_
-            FIXME: ^^^^^^
-        """
-        return getattr(self.block_open_api_instance, f'update_{self._block_snake_case}')(
-            body=self._BlockUpdate(**body),
-            **kwargs,
-            path_params={'branchId': self._branch_id, 'blockId': block_id},
-        )
+from .configuration import config
+from .utils import get_snake_and_pascal_case, parse_urllib_response
+from .settings import PACKAGE_NAME, CREATE, UPDATE
+from .branch import Branch
 
 
 class SedaroApiClient(ApiClient):
@@ -63,39 +20,59 @@ class SedaroApiClient(ApiClient):
             header_value=api_key
         )
 
-    def get_block_client(self, branch_id: int, block_name: str) -> BlockClient:
-        """Returns the api instance associated with the block corresponding to the `block_name` passed in.
+    def get_branch(self, branch_id: int) -> Branch:
+        """Gets a Sedaro Branch based on the give `branch_id`. Must be accessible to this `SedaroApiClient` via the
+        `api_key`.
 
         Args:
-            branch_id (int): id of the Sedaro `Branch` to interact with
-            block_name (str): name of an official Sedaro `Block`. Can use any standard string case that can be parsed to
-            pascal case (ie. `"BatteryCell"`, `"batteryCell"`, `"battery_cell"`, `"BATTERY_CELL"` all become
-            `"BatteryCell"`).
+            branch_id (int): the id of the desired Sedaro Branch
 
         Returns:
-            Api: the associated `Api` instance corresponding to the Sedaro `Block`
+            Branch: A `Branch` object that has various helpful methods on it.
         """
-        block_snake_case = snake_case(block_name)
-        block_pascal_case = pascal_case(block_name, strict=False)
-
-        block_api_module = import_module(f'{PACKAGE_NAME}.apis.tags.{block_snake_case}_api')
-        block_open_api_instance: Api = getattr(block_api_module, f'{block_pascal_case}Api')(self)
-
-        crud_module_path = f'{PACKAGE_NAME}.model.{block_snake_case}'
-
-        BlockCreate = getattr(
-            import_module(f'{crud_module_path}_create'),
-            f'{block_pascal_case}Create'
-        )
-        BlockUpdate = getattr(
-            import_module(f'{crud_module_path}_update'),
-            f'{block_pascal_case}Update'
+        res = self.call_api(f'/models/branches/{branch_id}', 'GET')
+        return Branch(
+            id=branch_id,
+            data=parse_urllib_response(res)['data'],
+            sedaro_client=self
         )
 
-        return BlockClient(
-            _block_snake_case=block_snake_case,
-            _BlockCreate=BlockCreate,
-            _BlockUpdate=BlockUpdate,
-            _branch_id=branch_id,
-            block_open_api_instance=block_open_api_instance
+    def _get_block_open_api_instance(self, block_name: str) -> Api:
+        """Get a open api block api instance that can be used to call various methods.
+
+        Args:
+            block_name (str): name of an official Sedaro `Block`
+
+        Returns:
+            Api: The corresponding block api instance instantiated with the Sedaro client passed in.
+        """
+        block_snake, block_pascal = get_snake_and_pascal_case(block_name)
+        block_api_module = import_module(f'{PACKAGE_NAME}.apis.tags.{block_snake}_api')
+        return getattr(block_api_module, f'{block_pascal}Api')(self)
+
+    def _get_create_or_update_block_model(self, block_name: str, create_or_update: Literal['create', 'update']):
+        """Gets the class to used to validate the data to create or update a `Block` 
+
+        Args:
+            block_name (str): name of an official Sedaro `Block`
+            create_or_update (Literal['create', 'update']): the action `'create'` or `'update'`
+
+        Raises:
+            ValueError: if you don't pass `'create'` or `'update'` for `create_or_update`
+
+        Returns:
+            _type_: _description_ FIXME
+        """
+        create_or_update = create_or_update.lower()
+        if create_or_update not in [CREATE, UPDATE]:
+            raise ValueError(
+                "The create_or_update arg must be either a string of either 'create' or 'update'.")
+
+        block_snake, block_pascal = get_snake_and_pascal_case(block_name)
+
+        crud_module_path = f'{PACKAGE_NAME}.model.{block_snake}'
+
+        return getattr(
+            import_module(f'{crud_module_path}_{create_or_update}'),
+            f'{block_pascal}{create_or_update.capitalize()}'
         )
