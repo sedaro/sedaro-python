74b48682a5f44826803d21e437946e70b309356a
Zachary Duvall <zachary.duvall@sedarotech.com>
2022-10-12 16:48:31 -0400

    Add helper function to pare response

--- sedaro_old/sedaro_old/custom/utils.py
+++ sedaro_old/sedaro_old/custom/utils.py
@@ -1,8 +1,14 @@
+import json
 from functools import cache
 from pydash.strings import snake_case, pascal_case
+from urllib3.response import HTTPResponse
 
 
 @cache
 def get_snake_and_pascal_case(s: str):
     '''Returns a tuple of the string in snake and pascal case'''
     return snake_case(s), pascal_case(s, strict=False)
+
+
+def parse_urllib_response(response: HTTPResponse):
+    return json.loads(response.data.decode('utf-8'))
