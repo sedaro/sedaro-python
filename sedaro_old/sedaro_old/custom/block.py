255c7556b4d04cdf9c5132ffcaf18b784cbf13ce
Zachary Duvall <zachary.duvall@sedarotech.com>
2022-10-12 19:28:44 -0400

    Access data from branch data

--- sedaro_old/sedaro_old/custom/block.py
+++ sedaro_old/sedaro_old/custom/block.py
@@ -8,11 +8,14 @@ if TYPE_CHECKING:
 @dataclass
 class Block:
     id: int
-    data: Dict
-    block_group_single_class: 'BlockClassClient'
+    block_class_client: 'BlockClassClient'
     '''Class for interacting with all Blocks of this class type'''
     block_group: str
     '''Corresponds to actual Sedaro BlockGroup name'''
 
     def __str__(self) -> str:
         return f'Block(id={self.id}, data={self.data}, block_group={self.block_group})'
+
+    @property
+    def data(self):
+        return self.block_class_client.branch.data[self.block_group][self.id]
