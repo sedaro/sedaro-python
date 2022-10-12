a02b2dfb7025a08544464a234a46d6289d461a89
Zachary Duvall <zachary.duvall@sedarotech.com>
2022-10-12 15:13:29 -0400

    Add cache, fix function name

--- sedaro_old/sedaro_old/custom/utils.py
+++ sedaro_old/sedaro_old/custom/utils.py
@@ -1,6 +1,8 @@
+from functools import cache
 from pydash.strings import snake_case, pascal_case
 
 
-def get_snake_and_camel_case(s: str):
-    '''Returns a tuple of the string in snake and camel case'''
+@cache
+def get_snake_and_pascal_case(s: str):
+    '''Returns a tuple of the string in snake and pascal case'''
     return snake_case(s), pascal_case(s, strict=False)
