1fca6ccec702deb468fec5aae76ac95943883243
Zachary Duvall <zachary.duvall@sedarotech.com>
2022-10-12 17:36:34 -0400

    Use Block class in erturn from create

--- sedaro_old/sedaro_old/custom/block_group_single_class.py
+++ sedaro_old/sedaro_old/custom/block_group_single_class.py
@@ -4,6 +4,8 @@ from typing import TYPE_CHECKING, Dict
 from pydash.strings import snake_case
 
 from .settings import CREATE
+from .block import Block
+from .utils import parse_urllib_response
 
 if TYPE_CHECKING:
     from .branch import Branch
@@ -11,6 +13,7 @@ if TYPE_CHECKING:
 
 @dataclass
 class BlockGroupSingleClass:
+    '''Class for interacting with all Blocks of this class type'''
     block_name: str
     block_openapi_instance: Api
     create_class: type
@@ -26,8 +29,17 @@ class BlockGroupSingleClass:
             _type_: _description_
             FIXME: ^^^^^^
         """
-        return getattr(self.block_openapi_instance, f'{CREATE}_{snake_case(self.block_name)}')(
+        res = getattr(self.block_openapi_instance, f'{CREATE}_{snake_case(self.block_name)}')(
             body=self.create_class(**body),
             **kwargs,
             path_params={'branchId': self.branch.id},
+        )
+        body = res.body
+        block_id, block_group = body['block']['id'], body['block']['group']
+        block_data = body['branch']['data'][block_group][block_id]
+        return Block(
+            id=block_id,
+            data=block_data,
+            block_group_single_class=self,
+            block_group=block_group
         )
