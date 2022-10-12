c94d18cf5288f2af337d38793e35ebb901063a1f
Zachary Duvall <zachary.duvall@sedarotech.com>
2022-10-12 16:52:38 -0400

    Add docstring and return type

--- sedaro_old/sedaro_old/custom/utils.py
+++ sedaro_old/sedaro_old/custom/utils.py
@@ -2,6 +2,7 @@ import json
 from functools import cache
 from pydash.strings import snake_case, pascal_case
 from urllib3.response import HTTPResponse
+from typing import Dict
 
 
 @cache
@@ -10,5 +11,6 @@ def get_snake_and_pascal_case(s: str):
     return snake_case(s), pascal_case(s, strict=False)
 
 
-def parse_urllib_response(response: HTTPResponse):
+def parse_urllib_response(response: HTTPResponse) -> Dict:
+    '''Parses the response from urllib3.response.HTTPResponse into a dictionary'''
     return json.loads(response.data.decode('utf-8'))
