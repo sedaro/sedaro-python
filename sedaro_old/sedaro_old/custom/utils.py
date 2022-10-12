13bb9f5143e40cbc4e711d04fe21bfd90e2ea73f
Zachary Duvall <zachary.duvall@sedarotech.com>
2022-10-12 14:59:26 -0400

    Add doc string to first util

--- sedaro_old/sedaro_old/custom/utils.py
+++ sedaro_old/sedaro_old/custom/utils.py
@@ -2,4 +2,5 @@ from pydash.strings import snake_case, pascal_case
 
 
 def get_snake_and_camel_case(s: str):
+    '''Returns a tuple of the string in snake and camel case'''
     return snake_case(s), pascal_case(s, strict=False)
