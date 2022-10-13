fd9979d2f18191c97d12fcce9f774518d26d421e
Zachary Duvall <zachary.duvall@sedarotech.com>
2022-10-12 20:59:30 -0400

    Add update method and some helper properties

--- sedaro_old/sedaro_old/custom/block.py
+++ sedaro_old/sedaro_old/custom/block.py
@@ -31,11 +31,42 @@ class Block:
         return self.block_class_client.block_name
 
     @property
+    def block_group(self) -> str:
+        '''The name of the Sedaro `BlockGroup` this type of `Block` is stored in'''
+        return self.block_class_client.block_group
+
+    @property
     def branch(self) -> 'Branch':
         '''The `Branch` this `Block` is connected to'''
         return self.block_class_client.branch
 
     @property
+    def block_openapi_instance(self) -> Api:
+        '''The api instance instantiated with the appropriate `SedaroApiClient` to interact with when CRUDing Blocks'''
+        return self.block_class_client.block_openapi_instance
+
+    @property
     def sedaro_client(self) -> 'SedaroApiClient':
         '''The `SedaroApiClient` this `Block` was accessed through'''
         return self.branch.sedaro_client
+
+    def update(self, body: Dict, **kwargs) -> 'Block':
+        """Update attributes of the `Block`
+
+        Args:
+            body (Dict): dictionary of attributes to update
+
+        Returns:
+            Block: updated version of self (previous reference's data is also updated)
+        """
+        self.block_class_client.update_class
+
+        body = self.data | body
+
+        res = getattr(self.block_openapi_instance, f'{UPDATE}_{snake_case(self.name)}')(
+            body=self.block_class_client.update_class(**body),
+            **kwargs,
+            path_params={'branchId': self.branch.id, "blockId": int(self.id)},
+        )
+        self.branch._process_block_crud_response(res)
+        return self
