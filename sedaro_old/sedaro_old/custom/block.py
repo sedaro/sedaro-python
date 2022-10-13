7540f8ca09fdd43ae687e66114bb5d39044bee35
Zachary Duvall <zachary.duvall@sedarotech.com>
2022-10-12 20:44:28 -0400

    Add imports, remove unneeded property

--- sedaro_old/sedaro_old/custom/block.py
+++ sedaro_old/sedaro_old/custom/block.py
@@ -1,5 +1,9 @@
 from typing import TYPE_CHECKING, Dict
 from dataclasses import dataclass
+from pydash import snake_case
+
+from sedaro_old.api_client import Api
+from .settings import UPDATE
 
 if TYPE_CHECKING:
     from .block_class_client import BlockClassClient
@@ -9,11 +13,9 @@ if TYPE_CHECKING:
 
 @dataclass
 class Block:
-    id: int
+    id: str
     block_class_client: 'BlockClassClient'
     '''Class for interacting with all Blocks of this class type'''
-    block_group: str
-    '''Corresponds to actual Sedaro BlockGroup name'''
 
     def __str__(self) -> str:
         return f'Block(id={self.id}, data={self.data}, block_group={self.block_group})'
