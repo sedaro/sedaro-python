48351a44da02fb786127e21f2026e543e8735b59
Zachary Duvall <zachary.duvall@sedarotech.com>
2022-10-12 21:46:42 -0400

    Update arg to id

--- sedaro_old/sedaro_old/custom/sedaro_api_client.py
+++ sedaro_old/sedaro_old/custom/sedaro_api_client.py
@@ -1,4 +1,5 @@
 from sedaro_old.api_client import ApiClient
+
 from .configuration import config
 from .utils import parse_urllib_response
 from .branch import Branch
@@ -14,19 +15,19 @@ class SedaroApiClient(ApiClient):
             header_value=api_key
         )
 
-    def get_branch(self, branch_id: int) -> Branch:
-        """Gets a Sedaro Branch based on the give `branch_id`. Must be accessible to this `SedaroApiClient` via the
+    def get_branch(self, id: int) -> Branch:
+        """Gets a Sedaro Branch based on the give `id`. Must be accessible to this `SedaroApiClient` via the
         `api_key`.
 
         Args:
-            branch_id (int): the id of the desired Sedaro Branch
+            id (int): the id of the desired Sedaro Branch
 
         Returns:
             Branch: A `Branch` object that has various helpful methods on it.
         """
-        res = self.call_api(f'/models/branches/{branch_id}', 'GET')
+        res = self.call_api(f'/models/branches/{id}', 'GET')
         return Branch(
-            id=branch_id,
+            id=id,
             data=parse_urllib_response(res)['data'],
             _sedaro_client=self
         )
