2e0357530fc874af738bca0037036d85c4278258
Zachary Duvall <zachary.duvall@sedarotech.com>
2022-11-21 17:05:33 -0500

    Refactor test file

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,24 +1,33 @@
 import time
 
+##### IMPORT AND ADD TESTS HERE ##############################
 import block_class_client_options
 import block_crud_tests
 import simulation_tests
 
+test_imports = [
+    block_class_client_options,
+    block_crud_tests,
+    simulation_tests
+]
+##############################################################
+
 
 def run_tests():
-    for file in [
-        block_class_client_options,
-        block_crud_tests,
-        simulation_tests
-    ]:
-        print(f'\n### "{file.__name__}" -- running tests')
+    num_tests = len(test_imports)
+    for i, t in enumerate(test_imports):
+
+        cur_t_num = f'{i + 1}/{num_tests}'
+
+        print(f'\n### Test {cur_t_num}: "{t.__name__}" -- running...')
         start_time = time.perf_counter()
 
-        file.run_tests()
+        t.run_tests()
 
         print(
-            f'\n### "{file.__name__}" -- done in {round(time.perf_counter() - start_time, 2)} seconds'
+            f'\n### Test {cur_t_num}: "{t.__name__}" -- done in {round(time.perf_counter() - start_time, 2)} seconds'
         )
+        time.sleep(.5)
 
     print('\n### Done!')
 
