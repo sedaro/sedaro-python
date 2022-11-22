75a318401e7fa23c2a16fcbc182a2866d96c6789
Zachary Duvall <zachary.duvall@sedarotech.com>
2022-11-22 10:29:36 -0500

    Update comment about config vars

--- tests/config.py
+++ tests/config.py
@@ -1,7 +1,8 @@
 HOST = 'http://localhost:80'
-# TODO: remove this
-# NOTE: update the API_KEY and WILDFIRE_A_T_ID for things that work with your dev environment
-# NOTE: these are temporary for Zach's dev environment
+
+# Update these variables below to be the API key of a user in your dev environment.
+# Also update the branch variables to the Wildfire Agent Template and Wildfire Scenario Branch id's that are connected
+# to that user.
 API_KEY = '1.jd8KABYSWWJ_VQ8dzW_78whf47VLhuHCn8X1n7PcGjK50zezvGSTE9vGWJEgVj5Xvo_FwOBGfITjrOJ5GDuwFQ'
 WILDFIRE_A_T_ID = 59
 WILDFIRE_SCENARIO_ID = 61
