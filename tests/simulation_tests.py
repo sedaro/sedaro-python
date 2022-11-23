c73240283005d1c5cf3ad20ff73fa717d2d7ec7b
zduvall <duvallzt@gmail.com>
2022-11-23 14:01:13 -0500

    Use SimClient methods

--- tests/simulation_tests.py
+++ tests/simulation_tests.py
@@ -13,25 +13,25 @@ def _check_job_status(job):
 def _test_run_simulation():
     with SedaroApiClient(api_key=API_KEY, host=HOST) as sedaro_client:
         # Instantiate job client
-        jobs_api_client = sedaro_client.get_job_api(WILDFIRE_SCENARIO_ID)
+        sim_client = sedaro_client.get_sim_client(WILDFIRE_SCENARIO_ID)
 
         # Start simulation
-        jobs_api_client.start_simulation()
+        sim_client.start()
         print('- Started simulation')
 
         # Get status #1
-        response = jobs_api_client.get_latest_simulation()
+        response = sim_client.get_latest()
         _check_job_status(response.body[0])
         time.sleep(3)
 
         # Get status #2
-        response = jobs_api_client.get_latest_simulation()
+        response = sim_client.get_latest()
         _check_job_status(response.body[0])
         time.sleep(3)
 
         # Terminate
         print('- Terminating...')
-        response = jobs_api_client.terminate_simulation(response.body[0]['id'])
+        response = sim_client.terminate(response.body[0]['id'])
         print('-', response.body['message'])
         assert response.body['message'] == 'Successfully terminated simulation.'
 
