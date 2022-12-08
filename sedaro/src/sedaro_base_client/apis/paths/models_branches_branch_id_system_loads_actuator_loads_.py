97715eada518789f46f8663ac9221ee21a924e41
zduvall <duvallzt@gmail.com>
2022-12-08 09:45:13 -0500

    Update base client, remove unused files

--- sedaro/src/sedaro_base_client/apis/paths/models_branches_branch_id_system_loads_actuator_loads_.py
+++
@@ -1,7 +0,0 @@
-from sedaro_base_client.paths.models_branches_branch_id_system_loads_actuator_loads_.post import ApiForpost
-
-
-class ModelsBranchesBranchIdSystemLoadsActuatorLoads(
-    ApiForpost,
-):
-    pass
