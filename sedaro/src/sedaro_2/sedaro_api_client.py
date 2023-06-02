e6213f1401b5a4b0936faf8c9c572afacf772c9f
zduvall <duvallzt@gmail.com>
2023-06-02 14:37:50 -0400

    Extend functionality to get specific branch clients

--- sedaro/src/sedaro_2/sedaro_api_client.py
+++ sedaro/src/sedaro_2/sedaro_api_client.py
@@ -6,7 +6,7 @@ from sedaro_base_client import Configuration
 from sedaro_base_client.api_client import ApiClient
 from sedaro_base_client.apis.tags import branches_api
 
-from .branch_client import BranchClient
+from .branch_clients import AgentTemplateBranch, BranchClient
 from .exceptions import SedaroApiException
 from .settings import COMMON_API_KWARGS
 from .sim_client import SimClient
@@ -16,29 +16,58 @@ from .utils import body_from_res, parse_urllib_response
 class SedaroApiClient(ApiClient):
     """A client to interact with the Sedaro API"""
 
-    def __init__(self, api_key, host='https://api.sedaro.com', *args, **kwargs):
+    def __init__(self, api_key, host='https://api.sedaro.com'):
         self._api_key = api_key
         self._api_host = host
-        self._api_args = args
-        self._api_kwargs = kwargs
 
+        # FIXME: remove this init when can
         return super().__init__(
             configuration=Configuration(host=host),
-            *args,
-            **kwargs,
             header_name='X_API_KEY',
             header_value=api_key
         )
 
     @contextmanager
     def api_client(self) -> Generator[ApiClient, Any, None]:
-        yield ApiClient(
+        """Instantiate ApiClient from sedaro_base_client
+
+        Yields:
+            Generator[ApiClient, Any, None]: ApiClient
+        """
+        with ApiClient(
             configuration=Configuration(host=self._api_host),
-            *self._api_args,
-            **self._api_kwargs,
             header_name='X_API_KEY',
             header_value=self._api_key
-        )
+        ) as api:
+            yield api
+
+    def __get_branch(self, branch_id: str) -> Dict:
+        """Get Sedaro `Branch` with given `branch_id` from `host`
+
+        Args:
+            branch_id (str): `id` of the Sedaro `Branch` to get
+
+        Returns:
+            Dict: `body` of the response as a `dict`
+        """
+        with self.api_client() as api:
+            branches_api_instance = branches_api.BranchesApi(api)
+            # res = branches_api_instance.get_branch(path_params={'branchId': id}) # TODO: temp_crud
+            # return BranchClient(res.body, self)
+            res = branches_api_instance.get_branch(
+                path_params={'branchId': branch_id}, **COMMON_API_KWARGS)
+            return body_from_res(res)
+
+    def agent_template_branch(self, branch_id: str) -> AgentTemplateBranch:
+        """Instantiate an `AgentTemplateBranch` client associated with the Sedaro `Branch` with `branch_id`
+
+        Args:
+            branch_id (str): `id` of the Sedaro Agent Template `Branch` to get
+
+        Returns:
+            AgentTemplateBranch: `AgentTemplateBranch` client
+        """
+        return AgentTemplateBranch(self.__get_branch(branch_id), self)
 
     def get_branch(self, id: int) -> BranchClient:
         """Gets a Sedaro Branch based on the given `id` and creates a `BranchClient` from the response. The branch must
