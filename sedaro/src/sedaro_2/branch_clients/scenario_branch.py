1d50f1039c09b0a20d6fe7ffe48921fa4756584a
zduvall <duvallzt@gmail.com>
2023-06-05 13:13:51 -0400

    Add simulation onto scenario branch

--- sedaro/src/sedaro_2/branch_clients/scenario_branch.py
+++ sedaro/src/sedaro_2/branch_clients/scenario_branch.py
@@ -5,6 +5,7 @@ from sedaro_base_client.paths.models_branches_branch_id.get import \
 
 from ..block_class_client import BlockClassClient
 from ..settings import SCENARIO_TEMPLATE
+from ..sim_client import SimClient
 from .branch_client import BranchClient
 
 if TYPE_CHECKING:
@@ -17,6 +18,16 @@ class ScenarioBranch(BranchClient):
         super().__init__(body, sedaro)
         if (type_ := self.data['type']) != SCENARIO_TEMPLATE:
             raise TypeError(f'Branch must be of type {SCENARIO_TEMPLATE} not {type_}')
+
+    def simulation(self) -> SimClient:
+        """Get a `SimClient` to interact with the simulation connected to this Sedaro scenario `Branch`.
+
+        Returns:
+            SimClient: a `SimClient`
+        """
+        # update methods inside sim client to call the context manager inside rather than expecting context manager
+        # to already be instantiated
+        return SimClient(self._sedaro, self.id)
 
     # ==============================================================================================================
     # For intellisense
