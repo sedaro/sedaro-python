932ea6d208a6ae06b34658a873081b2981bc2c78
zduvall <duvallzt@gmail.com>
2023-06-08 14:24:50 -0400

    Update comment

--- sedaro/src/sedaro/branches/blocks/block.py
+++ sedaro/src/sedaro/branches/blocks/block.py
@@ -35,7 +35,7 @@ class Block:
         return isinstance(other, self.__class__) and self.id == other.id
 
     def __hash__(self):
-        # allows a Block to be a key in a dict and @lru_cache wrapper to work on methods on this class
+        # allows a Block instance to be a key in a dictionary
         return hash(self.__class__.__name__ + self.id)
 
     def __getattr__(self, key: str) -> any:
