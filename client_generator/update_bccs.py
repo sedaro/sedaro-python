54d2d8a0875abf8bfe95718d381b236415d32c07
zduvall <duvallzt@gmail.com>
2023-10-10 11:22:29 -0400

    Add update bcc annotations

---
+++ client_generator/update_bccs.py
@@ -0,0 +1,42 @@
+import inspect
+from pathlib import Path
+
+from sedaro import SedaroApiClient
+
+API_KEY = 'API_KEY'
+HOST = 'http://localhost:80'
+AGENT_TEMPLATE_ID = 'SIMPLESAT_A_T_ID'
+SCENARIO_ID = 'SIMPLESAT_SCENARIO_ID'
+
+sedaro = SedaroApiClient(api_key=API_KEY, host=HOST)
+
+
+def update_branch_bcc_annotations():
+    for get_method, branch_id in (
+        (sedaro.agent_template, AGENT_TEMPLATE_ID),
+        (sedaro.scenario, SCENARIO_ID)
+    ):
+        branch = get_method(branch_id)
+        branch_kls = branch.__class__
+        class_file = inspect.getfile(branch_kls)
+        class_path = Path(class_file).relative_to(Path.cwd())
+
+        with class_path.open('r+') as f:
+            while line := f.readline():
+                if '$AUTO_EDIT_START$' in line:
+                    f.seek(0, 1)
+                    f.truncate()
+                    break
+            else:
+                raise ValueError('Unable to find $AUTO_EDIT_START$ in file')
+
+            f.write('\n')
+            a_or_an = 'an' if branch_kls.__name__[0].lower() in 'aeiou' else 'a'
+            doc_string = f'A Sedaro `Block` class on {a_or_an} `{branch_kls.__name__}`'
+            for block_name in sorted(branch.data['_blockNames']):
+                f.write(f'    {block_name}: BlockType\n')
+                f.write(f'    """{doc_string}"""\n')
+
+
+if __name__ == '__main__':
+    update_branch_bcc_annotations()
