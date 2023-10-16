1cbc542bab0f70f73aea8f865e813ee4d80bb1fc
Richard Tuminello <richard.tuminello@sedarotech.com>
2023-10-16 14:25:12 -0700

    get listed blocks from class

--- tests/test_bcc_options.py
+++ tests/test_bcc_options.py
@@ -1,131 +1,20 @@
 from config import API_KEY, HOST, SIMPLESAT_A_T_ID, SIMPLESAT_SCENARIO_ID
 
 from sedaro import SedaroApiClient
+from sedaro.branches import AgentTemplateBranch, ScenarioBranch
 from sedaro.branches.blocks import Block, BlockType
 
 sedaro = SedaroApiClient(api_key=API_KEY, host=HOST)
 
 
-# TODO: if update these lists, also update type hints of respective AgentTemplateBranch
-agent_template_blocks = [
-    'AngularVelocitySensor',
-    'Antenna',
-    'AveragingAlgorithm',
-    'Battery',
-    'BatteryCell',
-    'BatteryPack',
-    'BodyFrameVector',
-    'BodyInFovCondition',
-    'BusRegulator',
-    'CelestialTarget',
-    'CelestialVector',
-    'CircularFieldOfView',
-    'Component',
-    'ComponentToScalarCondition',
-    'CompoundCondition',
-    'Cooler',
-    'CooperativeTransmitInterface',
-    'DataBus',
-    'DataInterface',
-    'DataMode',
-    'DataStorage',
-    'DataType',
-    'DirectionSensor',
-    'EkfAlgorithm',
-    'FixedSurface',
-    'FuelReservoir',
-    'FullyRegDetPowerProcessor',
-    'GpsAlgorithm',
-    'GroundTarget',
-    'Heater',
-    'InternalDataInterface',
-    'LaserCommModule',
-    'LoadState',
-    'LocalVector',
-    'LockPointingMode',
-    'Magnetorquer',
-    'MaxAlignPointingMode',
-    'MekfAlgorithm',
-    'Modem',
-    'SpacecraftOperationalMode',
-    'OpticalAttitudeSensor',
-    'Orbit',
-    'PassivePointingMode',
-    'PassiveTransmitInterface',
-    'PidAlgorithm',
-    'PositionSensor',
-    'PowerLoad',
-    'PhotovoltaicPowerProcessor',
-    'QuasiRegDetPowerProcessor',
-    'ReactionWheel',
-    'ReceiveInterface',
-    'RectangularFieldOfView',
-    'ResistanceLoad',
-    'SameTargetMultiCondition',
-    'SatelliteToSatelliteCondition',
-    'SatelliteToScalarCondition',
-    'SatelliteToTargetCondition',
-    'ScanFieldOfViewArticulationMode',
-    'SingleConvHybridPowerProcessor',
-    'SingleConvMpptPowerProcessor',
-    'SlidingModeAlgorithm',
-    'SolarArray',
-    'SolarCell',
-    'SolarPanel',
-    'SpaceTarget',
-    'SphericalFuelTank',
-    'SpherocylinderFuelTank',
-    'StaticFieldOfViewArticulationMode',
-    'StaticThrustControlAlgorithm',
-    'Subsystem',
-    'SunTrackingSurface',
-    'SurfaceMaterial',
-    'TargetGroup',
-    'TargetGroupInFovCondition',
-    'TargetGroupToSatelliteCondition',
-    'TargetGroupToScalarCondition',
-    'TargetGroupToTargetCondition',
-    'TargetGroupVector',
-    'TargetInFovCondition',
-    'TargetToScalarCondition',
-    'TargetToTargetCondition',
-    'TargetVector',
-    'TempControllerState',
-    'ThermalDesignLayout',
-    'ThermalInterface',
-    'ThermalInterfaceMaterial',
-    'Thruster',
-    'TimeCondition',
-    'TrackingFieldOfViewArticulationMode',
-    'TriadAlgorithm',
-    'TwoConvMpptPowerProcessor',
-    'VectorInFovCondition',
-    'VectorSensor',
-    'VectorTrackingSurface'
-]
-
-# TODO: if update these lists, also update type hints of ScenarioBranch
-scenario_blocks = [
-    'Agent',
-    'AgentGroup',
-    'ClockConfig',
-    'Orbit',
-    'PerRoundExternalState',
-    'SpontaneousExternalState',
-    'WaypointPathWithDuration',
-    'WaypointPathWithTimestamps',
-    'WaypointPathWithSpeed'
-]
-
-
 def test_block_type_options():
-    for get_method, branch_id, _expected_block_names in [
-        [sedaro.agent_template, SIMPLESAT_A_T_ID, agent_template_blocks],
-        [sedaro.scenario, SIMPLESAT_SCENARIO_ID, scenario_blocks]
+    for get_method, branch_id, TemplateBranch in [
+        [sedaro.agent_template, SIMPLESAT_A_T_ID, AgentTemplateBranch],
+        [sedaro.scenario, SIMPLESAT_SCENARIO_ID, ScenarioBranch]
     ]:
+        expected_block_names = sorted([k for k,v in TemplateBranch.__dict__['__annotations__'].items() if v == BlockType])
         branch = get_method(branch_id)
         branch_block_names = sorted(branch.data['_blockNames'])
-        expected_block_names = sorted(_expected_block_names)
         # CHECK: lists above are correct
         assert expected_block_names == branch_block_names, f'Extra: {set(expected_block_names) - set(branch_block_names)}, Missing: {set(branch_block_names) - set(expected_block_names)}'
 
