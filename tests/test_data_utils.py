a44841b892332b6eafbfc99c7003b7529eaa52b0
Connor Williams <connor.williams@sedarotech.com>
2023-11-07 15:23:51 -0500

    more tests, and refactor

--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -2,7 +2,7 @@ from sedaro.branches.scenario_branch.sim_client import \
      __set_nested, set_nested, set_numeric_as_list, update_metadata, concat_results, concat_stream, concat_stream_data
 from sedaro.results.utils import to_time_major
 
-def test_set_nested():
+def test_set_nested_and_numeric():
     assert __set_nested({'foo.bar': [1, 2, 3], 'foo.baz': [1, 2, 3]}) == {'foo': {'bar': [1, 2, 3], 'baz': [1, 2, 3]}}
     assert __set_nested({'foo': [1, 2, 3], 'foo.bar': [1, 2, 3], 'foo.bar.baz': [1, 2, 3], 'foo.bar.baz.qux': [1, 2, 3]}) == \
          {'foo': {'bar': {'baz': {'qux': [1, 2, 3]}}}}
@@ -13,20 +13,10 @@ def test_set_nested():
         'foo/0': ([1, 2, 3], {'foo': {'bar': {'baz': [1, 2, 3]}, 'qux': {'corge': [2, 3, 4]}}}),
         'foo/1': ([1, 2, 3, 4, 5], {'foo': {'bar': {'baz': [1, 2, 3, 4, 5], 'qux': [2, 3, 4, 5, 6]}}})
     }
+    assert set_numeric_as_list({'a': [1, 2], 'b': {'0': [1, 2], '1': [1, 2]}}) == {'a': [1, 2], 'b': [[1, 2], [1, 2]]}
+    assert set_numeric_as_list({'a': [1, 2], 'b': {'zero': [1, 2], 'one': [1, 2]}}) == {'a': [1, 2], 'b': {'zero': [1, 2], 'one': [1, 2]}}
 
-def test_set_numeric_as_list():
-    pass
-
-def test_update_metadata():
-    pass
-
-def test_concat_results():
-    pass
-
-def test_concat_stream():
-    pass
-
-def test_concat_stream_data():
+def test_concat_and_update():
     pass
 
 def test_to_time_major():
@@ -37,12 +27,8 @@ def test_to_time_major():
          [[[1, 2], [3, 4]], [[5, 6], [7, 8]], [[9, 10], [11, 12]]]
 
 def run_tests():
-    test_set_nested()
-    test_set_numeric_as_list()
-    test_update_metadata()
-    test_concat_results()
-    test_concat_stream()
-    test_concat_stream_data()
+    test_set_nested_and_numeric()
+    test_concat_and_update()
     test_to_time_major()
 
 run_tests()
