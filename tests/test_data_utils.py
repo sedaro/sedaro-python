470f5f15eea043b92ccd43fde73402ff589522e9
Connor Williams <connor.williams@sedarotech.com>
2023-11-07 15:11:27 -0500

    tests for set_nested

--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -1,8 +1,18 @@
-from sedaro.branches.scenario_branch.sim_client import set_nested, set_numeric_as_list, update_metadata, concat_results, concat_stream, concat_stream_data
+from sedaro.branches.scenario_branch.sim_client import \
+     __set_nested, set_nested, set_numeric_as_list, update_metadata, concat_results, concat_stream, concat_stream_data
 from sedaro.results.utils import to_time_major
 
 def test_set_nested():
-    pass
+    assert __set_nested({'foo.bar': [1, 2, 3], 'foo.baz': [1, 2, 3]}) == {'foo': {'bar': [1, 2, 3], 'baz': [1, 2, 3]}}
+    assert __set_nested({'foo': [1, 2, 3], 'foo.bar': [1, 2, 3], 'foo.bar.baz': [1, 2, 3], 'foo.bar.baz.qux': [1, 2, 3]}) == \
+         {'foo': {'bar': {'baz': {'qux': [1, 2, 3]}}}}
+    d = {'foo/0': ([1, 2, 3], {'foo': {'bar.baz': [1, 2, 3], 'qux.corge': [2, 3, 4]}})}
+    assert set_nested(d) == {'foo/0': ([1, 2, 3], {'foo': {'bar': {'baz': [1, 2, 3]}, 'qux': {'corge': [2, 3, 4]}}})}
+    d['foo/1'] = ([1, 2, 3, 4, 5], {'foo': {'bar.baz': [1, 2, 3, 4, 5], 'bar.qux': [2, 3, 4, 5, 6]}})
+    assert set_nested(d) == {
+        'foo/0': ([1, 2, 3], {'foo': {'bar': {'baz': [1, 2, 3]}, 'qux': {'corge': [2, 3, 4]}}}),
+        'foo/1': ([1, 2, 3, 4, 5], {'foo': {'bar': {'baz': [1, 2, 3, 4, 5], 'qux': [2, 3, 4, 5, 6]}}})
+    }
 
 def test_set_numeric_as_list():
     pass
@@ -27,6 +37,12 @@ def test_to_time_major():
          [[[1, 2], [3, 4]], [[5, 6], [7, 8]], [[9, 10], [11, 12]]]
 
 def run_tests():
+    test_set_nested()
+    test_set_numeric_as_list()
+    test_update_metadata()
+    test_concat_results()
+    test_concat_stream()
+    test_concat_stream_data()
     test_to_time_major()
 
 run_tests()
