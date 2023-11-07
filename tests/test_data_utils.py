906fed6d9c0e91663068e832a8ed2e4cbeb92041
Connor Williams <connor.williams@sedarotech.com>
2023-11-07 14:49:18 -0500

    better 3d major axis test

--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -4,7 +4,8 @@ def test_to_time_major():
     assert to_time_major(True) == True # items other than lists or dicts are returned as is
     assert to_time_major([1, 2, 3]) == [1, 2, 3]
     assert to_time_major([[1, 2], [3, 4], [5, 6]]) == ([[1, 3, 5], [2, 4, 6]])
-    assert to_time_major([[[1, 5], [2, 6]], [[3, 7], [4, 8]]]) == [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
+    assert to_time_major([[[1, 5, 9], [2, 6, 10]], [[3, 7, 11], [4, 8, 12]]]) == \
+         [[[1, 2], [3, 4]], [[5, 6], [7, 8]], [[9, 10], [11, 12]]]
 
 def run_tests():
     test_to_time_major()
