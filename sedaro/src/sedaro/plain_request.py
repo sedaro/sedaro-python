02d447806f38946cae642771078bb9859b78f3f3
Bas Welsh <sebastian.welsh@sedarotech.com>
2024-04-02 19:28:03 -0400

    Update plain_request.py

--- sedaro/src/sedaro/plain_request.py
+++ sedaro/src/sedaro/plain_request.py
@@ -72,7 +72,7 @@ class PlainRequest:
             kwargs['headers']['X-CSRFToken'] = self.__sedaro._csrf_token
 
         if self.__sedaro._proxy_url and self.__sedaro._proxy_url.startswith('http'):
-            protocol = self.__sedaro._proxy_url.split('://')[0]
+            protocol = self.__sedaro._api_host.split('://')[0]
             kwargs['proxies'] = {protocol: self.__sedaro._proxy_url}
             kwargs['headers'] |= (self.__sedaro._proxy_headers or {})
 
