3a55a10868991ab8ec2f664ea595c2ac112ddf75
Daniel Zhao <68676272+el-agua@users.noreply.github.com>
2024-08-22 13:34:58 -0400

    Remove prints and add logging debug

--- sedaro/src/sedaro/grpc_client/cosim_client.py
+++ sedaro/src/sedaro/grpc_client/cosim_client.py
@@ -8,8 +8,9 @@ from . import cosim_pb2
 from . import cosim_pb2_grpc
 import json
 import numpy as np
+import os
 import requests
-import settings
+from sedaro.branches.scenario_branch.sim_client import serdes as serdes
 
 
 def deser(v):
@@ -53,6 +54,8 @@ class MessageGenerator:
                 self._local_storage["auth"] = response
                 self._variable_blocker["auth"].set()
             else:
+                if response.state == "False":
+                    raise Exception("Error: " + response.value)
                 if response.state == "EXPIRED":
                     self._expired = True
                 if response.action == cosim_pb2.CosimActionType.COSIM_ACTION_CONSUME:
@@ -67,44 +70,49 @@ class MessageGenerator:
                     self._variable_produce_blocker[response.external_state_block_id].set(
                     )
 
-        print("END")
-
-    def consume(self, x, agent_id):
+    def consume(self, x, agent_id, time):
         if x not in self._variable_consume_blocker:
             self._variable_consume_blocker[x] = threading.Event()
         else:
             self._variable_consume_blocker[x].clear()
-        self._send(cosim_pb2.CosimRequest(action=cosim_pb2.CosimActionType.COSIM_ACTION_CONSUME,
-                                          external_state_block_id=x, agent_id=agent_id))
+        if time:
+            self._send(cosim_pb2.CosimRequest(action=cosim_pb2.CosimActionType.COSIM_ACTION_CONSUME,
+                                          external_state_block_id=x, agent_id=agent_id, time=time))
+        else:
+            self._send(cosim_pb2.CosimRequest(action=cosim_pb2.CosimActionType.COSIM_ACTION_CONSUME,
+                                            external_state_block_id=x, agent_id=agent_id))
         self._variable_consume_blocker[x].wait()
         consumed_value = self._local_storage[x]
         if self._expired:
             if self._auth_payload:
                 refresh = self.authenticate(self._auth_payload["api_key"], self._auth_payload["address"], self._auth_payload["job_id"])
                 if refresh:
-                    self._expired = True
+                    self._expired = False
                     return self.consume(x, agent_id)
                 else:
                     raise Exception("Authentication failed")
             else:
                     raise Exception("Authentication failed")
-        print()
         return serdes(deser(consumed_value.value))
 
-    def produce(self, x, agent_id, value):
+    def produce(self, x, agent_id, value, time):
         if x not in self._variable_produce_blocker:
             self._variable_produce_blocker[x] = threading.Event()
         else:
             self._variable_produce_blocker[x].clear()
         new_value = json.dumps({"payload": serdes(value)})
-        self._send(cosim_pb2.CosimRequest(action=cosim_pb2.CosimActionType.COSIM_ACTION_PRODUCE,
-                   external_state_block_id=x, agent_id=agent_id, value=new_value))
+        if time:
+            self._send(cosim_pb2.CosimRequest(action=cosim_pb2.CosimActionType.COSIM_ACTION_PRODUCE,
+                    external_state_block_id=x, agent_id=agent_id, value=new_value, time=time))
+        else:
+            self._send(cosim_pb2.CosimRequest(action=cosim_pb2.CosimActionType.COSIM_ACTION_PRODUCE,
+                    external_state_block_id=x, agent_id=agent_id, value=new_value))
         self._variable_produce_blocker[x].wait()
         if self._expired:
             if self._auth_payload:
                 refresh = self.authenticate(self._auth_payload["api_key"], self._auth_payload["address"], self._auth_payload["job_id"])
                 if refresh:
-                    self._expired = True
+                    self._expired = False
                     return self.produce(x, agent_id, value)
                 else:
                     raise Exception("Authentication failed")
@@ -115,9 +123,8 @@ class MessageGenerator:
     def authenticate(self, api_key, address, job_id, host):
         self._variable_blocker["auth"] = threading.Event()
         res = requests.get(f"http://{host}/simulations/jobs/{job_id}/authorization?audience={"SimBed"}&permission=RUN_SIMULATION", headers={"X_API_KEY": api_key})
-        print(res.json())
         self._send(cosim_pb2.CosimRequest(
-            auth_token=cosim_pb2.AuthMETA(api_key=api_key, auth_token=res.json()['jwt']), action=cosim_pb2.CosimActionType.COSIM_ACTION_AUTHENTICATE, cluster_handle_address=address, job_id=job_id))
+            auth_token=cosim_pb2.AuthMeta(auth_token=res.json()['jwt']), action=cosim_pb2.CosimActionType.COSIM_ACTION_AUTHENTICATE, cluster_handle_address=address, job_id=job_id))
         self._variable_blocker["auth"].wait()
         success = self._local_storage["auth"]
         valid = (success.state == "True")
@@ -128,17 +135,16 @@ class MessageGenerator:
     def terminate(self):
         self.close_stream.set()
         self.queue = []
-        print("TERMINATING...")
+        logging.debug("TERMINATING...")
         return
 
     def _send(self, message):
-        print("SENDING")
+        logging.debug("SENDING")
         self.queue.append(message)
 
     def _stream(self):
         while not self.close_stream.is_set():
             if self.queue:
-                print("SENDING MESSAGE")
                 new_message = self.queue.pop(0)
                 yield new_message
 
@@ -152,29 +158,24 @@ class CosimRunner:
         self._hold_stream = threading.Event()
         self._error = None
         
-    def _open(self, api_key, address, job_id, host):
+    def _open(self, api_key, address, job_id, host, grpc_host):
         try:
-            root_certs = settings.CLIENT_ROOT_CERT
-            if not root_certs:
-                self._error = e
-                self._hold_stream.set()
-                raise Exception("No root certificate provided")
-            credentials = grpc.ssl_channel_credentials(root_certs)
+            credentials = grpc.ssl_channel_credentials()
         except Exception as e:
-            print("Error loading CA certificate: " + str(e))
+            logging.debug("Error loading CA certificate: " + str(e))
             self._error = e
             self._hold_stream.set()
             raise Exception("Error loading CA certificate: " + str(e))
         try:
-            with grpc.secure_channel('localhost:50049', credentials) as channel:
+            with grpc.secure_channel(grpc_host, credentials) as channel:
                 message_generator = MessageGenerator(
                     ThreadPoolExecutor(max_workers=10), channel)
                 message_generator.open_stream()
-                print("Stream opened")
+                logging.debug("Stream opened")
                 success = message_generator.authenticate(
                     api_key, address, job_id, host)
                 if not success:
-                    print("Authentication failed")
+                    logging.debug("Authentication failed")
                     message_generator.terminate()
                     raise Exception("Authentication failed")
                 self.message_generator = message_generator
--- sedaro/src/sedaro/grpc_client/cosim_pb2_grpc.py
+++ sedaro/src/sedaro/grpc_client/cosim_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import cosim_pb2 as cosim__pb2
+from . import cosim_pb2 as cosim__pb2
 
 GRPC_GENERATED_VERSION = '1.64.1'
 GRPC_VERSION = grpc.__version__
