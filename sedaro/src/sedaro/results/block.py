b5a11cd859e60592e6d7d3a113e07ec1a8d9a8c1
Connor Williams <connor.williams@sedarotech.com>
2024-10-30 13:53:42 -0400

    better wording

--- sedaro/src/sedaro/results/block.py
+++ sedaro/src/sedaro/results/block.py
@@ -209,8 +209,8 @@ class SedaroBlockResult(FromFileAndToFileAreDeprecated):
         if self.__static_data:
             hfill()
             print("📦 Static data is available for this block.")
-            print("📦 Query with .static_data('<ENGINE_NAME>') to get static data for that engine,")
-            print("or .static_data() to get static data for all engines.")
+            print("📦 Query with .static_data('<ENGINE_NAME>') for that engine's static data on this block,")
+            print("or .static_data() to get this block's static data for all engines.")
 
     def value_at(self, mjd):
         return {variable: self.__getattr__(variable).value_at(mjd) for variable in self.variables}
