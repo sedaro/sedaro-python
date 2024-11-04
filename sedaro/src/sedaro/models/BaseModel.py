6a88d37092af49712d1859e21f1d1ee45e693c0e
Zachary Duvall <zachary.duvall@sedarotech.com>
2024-11-04 11:08:32 -0500

    Add BaseModel

---
+++ sedaro/src/sedaro/models/BaseModel.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+from sedaro.settings import ID
+
+if TYPE_CHECKING:
+    from ..sedaro_api_client import SedaroApiClient
+
+
+@dataclass
+class BaseModel(ABC):
+    _raw_data: 'dict'
+    _sedaro: 'SedaroApiClient'
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id})"
+
+    @property
+    def id(self):
+        return self._raw_data[ID]
