1c0eb7c8624ad722fe58c86c8806e515400878ec
Zachary Duvall <zachary.duvall@sedarotech.com>
2024-11-04 14:45:34 -0500

    Update workspace manager

--- sedaro/src/sedaro/models/workspace/WorkspaceManager.py
+++ sedaro/src/sedaro/models/workspace/WorkspaceManager.py
@@ -4,7 +4,7 @@ from ..BaseModelManager import BaseModelManager
 from .Workspace import Workspace
 
 
-class WorkspaceManager(BaseModelManager):
+class WorkspaceManager(BaseModelManager[Workspace]):
 
     _BASE_PATH: 'ClassVar[str]' = '/workspaces'
     _MODEL: 'ClassVar[type[Workspace]]' = Workspace
