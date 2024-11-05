6d5a519e91932ec749e5a0a00f7ae5895436c577
Zachary Duvall <zachary.duvall@sedarotech.com>
2024-11-05 10:00:36 -0500

    Update tests

--- tests/test_model_managers.py
+++ tests/test_model_managers.py
@@ -5,7 +5,9 @@ from config import API_KEY, HOST
 
 from sedaro import SedaroApiClient
 from sedaro.exceptions import SedaroApiException
+from sedaro.models.repository.Repository import Repository
 from sedaro.models.workspace.Workspace import Workspace
+from sedaro.settings import SCENARIO
 
 
 def test_workspace():
@@ -16,31 +18,72 @@ def test_workspace():
     assert all(isinstance(w, Workspace) for w in workspaces1)
 
     w_new = sedaro.Workspace.create(name='1')
-    assert (isinstance(w_new, Workspace))
+    try:
+        assert (isinstance(w_new, Workspace))
 
-    time.sleep(4)  # need time for newly created workspace to be retreivable
+        time.sleep(4)  # need time for newly created workspace to be retreivable
 
-    workspaces2 = sedaro.Workspace.get()
-    assert len(workspaces2) == len(workspaces1) + 1
+        workspaces2 = sedaro.Workspace.get()
+        assert len(workspaces2) == len(workspaces1) + 1
 
-    w_new2 = sedaro.Workspace.get(w_new.id)
-    assert (isinstance(w_new2, Workspace))
-    assert w_new2.id == w_new.id
+        w_new2 = sedaro.Workspace.get(w_new.id)
+        assert (isinstance(w_new2, Workspace))
+        assert w_new2.id == w_new.id
 
-    w_new.update(name='2')
-    assert w_new.name == '2'
-    assert w_new2.name != w_new.name
+        w_new.update(name='2')
+        assert w_new.name == '2'
+        assert w_new2.name != w_new.name
 
-    w_new.refresh()
-    assert w_new.name == w_new.name
+        w_new.refresh()
+        assert w_new.name == w_new.name
 
-    w_new.delete()
+    finally:
+        w_new.delete()
 
     with pytest.raises(
         SedaroApiException,
         match="RESOURCE_NOT_FOUND: The requested resource does not exist or is not accessible"
     ):
         sedaro.Workspace.get(w_new.id)
+
+
+def test_repository():
+    sedaro = SedaroApiClient(api_key=API_KEY, host=HOST)
+
+    w_new = sedaro.Workspace.create(name='1')
+
+    try:
+        repos = sedaro.Repository.get()
+        assert all(isinstance(r, Repository) for r in repos)
+        repo_new = sedaro.Repository.create(name='1', metamodelType=SCENARIO, workspace=w_new.id)
+        assert isinstance(repo_new, Repository)
+        assert repo_new.metamodelType == SCENARIO
+        assert repo_new.workspace['id'] == w_new.id
+        assert repo_new.name == '1'
+
+        repo_new2 = sedaro.Repository.get(repo_new.id)
+        assert repo_new2.id == repo_new.id
+
+        repo_new.update(name='2')
+        assert repo_new.name == '2'
+        assert repo_new2.name != repo_new.name
+        repo_new2.refresh()
+        assert repo_new2.name == repo_new.name
+
+        all_repos = sedaro.Repository.get()
+
+        repo_new.delete()
+
+        assert len(sedaro.Repository.get()) == len(all_repos) - 1
+
+        with pytest.raises(
+            SedaroApiException,
+            match="RESOURCE_NOT_FOUND: The requested resource does not exist or is not accessible"
+        ):
+            repo_new.refresh()
+
+    finally:
+        w_new.delete()
 
 
 def run_tests():
