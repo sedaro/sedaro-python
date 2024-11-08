833c564a8385f41f841cf9e2bb3584fbd17c2500
Zachary Duvall <zachary.duvall@sedarotech.com>
2024-11-08 16:26:28 -0500

    Add import

--- sedaro/src/sedaro/sedaro_api_client.py
+++ sedaro/src/sedaro/sedaro_api_client.py
@@ -9,6 +9,7 @@ from urllib3.exceptions import InsecureRequestWarning
 from urllib3.response import HTTPResponse
 
 from .branches import AgentTemplateBranch, Branch, ScenarioBranch
+from .models.project.ProjectManager import ProjectManager
 from .models.repository.RepositoryManager import RepositoryManager
 from .models.workspace.WorkspaceManager import WorkspaceManager
 from .plain_request import PlainRequest
