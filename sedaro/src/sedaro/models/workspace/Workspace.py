ecb15e99961b084c4d159d2f5a795103af9c5844
Zachary Duvall <zachary.duvall@sedarotech.com>
2024-11-11 12:45:50 -0500

    Add projects property

--- sedaro/src/sedaro/models/workspace/Workspace.py
+++ sedaro/src/sedaro/models/workspace/Workspace.py
@@ -1,5 +1,15 @@
+from typing import TYPE_CHECKING
+
 from ..BaseModel import BaseModel
+
+if TYPE_CHECKING:
+    from ..project.Project import Project
 
 
 class Workspace(BaseModel):
     pass
+
+    @property
+    def projects(self) -> 'list[Project]':
+        from ..project.Project import Project
+        return self._get_rel('projects', Project)
