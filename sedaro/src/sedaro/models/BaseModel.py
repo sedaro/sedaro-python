4e0f9da00a8f97e3a1acb81f357afc83e61d035a
Zachary Duvall <zachary.duvall@sedarotech.com>
2024-11-11 12:44:21 -0500

    Add _get_rel method

--- sedaro/src/sedaro/models/BaseModel.py
+++ sedaro/src/sedaro/models/BaseModel.py
@@ -42,3 +42,17 @@ class BaseModel(ABC):
     def _delete(self, *, query_params: dict = None):
         mod_man = self._model_manager
         mod_man._sedaro.request.delete(mod_man._req_url(id=self.id, query_params=query_params))
+
+    def _get_rel(self, field: 'str', base_model_type: 'type[M]') -> 'M' | list[M]:
+        '''Get the related model(s) of the given field.'''
+        from .BaseModelManager import BaseModelManager
+
+        if (id_or_ids := self._raw_data.get(field)) in (None, []):
+            return id_or_ids
+
+        mod_man_cls = BaseModelManager._model_to_model_manager[base_model_type]
+        mod_man = mod_man_cls(self._model_manager._sedaro)
+
+        if isinstance(id_or_ids, list):
+            return [mod_man.get(id_) for id_ in id_or_ids]
+        return mod_man.get(id_or_ids)
