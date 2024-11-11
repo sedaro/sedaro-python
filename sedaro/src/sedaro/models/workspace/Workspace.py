682e50454104ec0f30eb4ab54c70543bb5f5889a
Zachary Duvall <zachary.duvall@sedarotech.com>
2024-11-11 12:56:10 -0500

    Update docstrings

--- sedaro/src/sedaro/models/workspace/Workspace.py
+++ sedaro/src/sedaro/models/workspace/Workspace.py
@@ -12,10 +12,12 @@ class Workspace(BaseModel):
 
     @property
     def projects(self) -> 'list[Project]':
+        '''Get the projects of the workspace.'''
         from ..project.Project import Project
         return self._get_rel('projects', Project)
 
     @property
     def repositories(self) -> 'list[Repository]':
+        '''Get the repositories of the workspace.'''
         from ..repository.Repository import Repository
         return self._get_rel('repositories', Repository)
