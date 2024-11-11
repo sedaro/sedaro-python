e7c9301a8f34e704394be29495820502812f1b05
Zachary Duvall <zachary.duvall@sedarotech.com>
2024-11-11 12:54:32 -0500

    Add workspace relationship property

--- sedaro/src/sedaro/models/repository/Repository.py
+++ sedaro/src/sedaro/models/repository/Repository.py
@@ -1,5 +1,15 @@
+from typing import TYPE_CHECKING
+
 from ..BaseModel import BaseModel
+
+if TYPE_CHECKING:
+    from ..workspace.Workspace import Workspace
 
 
 class Repository(BaseModel):
     pass
+
+    @property
+    def workspace(self) -> 'Workspace':
+        from ..workspace.Workspace import Workspace
+        return self._get_rel('workspace', Workspace)
