06086c3edf245d43e138e89224d8a46bb42e0e82
Zachary Duvall <zachary.duvall@sedarotech.com>
2024-11-11 12:48:48 -0500

    Add repository relationship property

--- sedaro/src/sedaro/models/workspace/Workspace.py
+++ sedaro/src/sedaro/models/workspace/Workspace.py
@@ -4,6 +4,7 @@ from ..BaseModel import BaseModel
 
 if TYPE_CHECKING:
     from ..project.Project import Project
+    from ..repository.Repository import Repository
 
 
 class Workspace(BaseModel):
@@ -13,3 +14,8 @@ class Workspace(BaseModel):
     def projects(self) -> 'list[Project]':
         from ..project.Project import Project
         return self._get_rel('projects', Project)
+
+    @property
+    def repositories(self) -> 'list[Repository]':
+        from ..repository.Repository import Repository
+        return self._get_rel('repositories', Repository)
